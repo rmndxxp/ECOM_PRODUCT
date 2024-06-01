cada104fc45a885623a1072c7bef38e82e285bd0
Ramandeep <ramandeep1568@conestogac.on.ca>
2024-06-01 01:49:21 -0400

    Added Product Class

---
+++ ECOM_PRODUCT/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECOM_PRODUCT
+{
+    public class Product
+    {
+        //Declaring Variables(Required Attributes)
+        public int ProductID {  get; set; }
+        public string ProductName { get; set; }
+        public decimal Price {  get; set; }
+        public int Stock {  get; set; }
+        public Product(int  productID, string productName, decimal price,int stock)
+        {
+            ProductID = productID;
+            ProductName = productName;
+            Price = price;
+            Stock = stock;
+        }
+        //method to increase stock
+        public void IncreaseStock(int quantity)
+        {
+            if(quantity<0)
+            {
+                throw new ArgumentException("Quantity cannot be Empty");
+            }
+            else
+            {
+                Stock += quantity;
+            }
+        }
+        //method to decrease stock and verify quantity
+        public void DecreaseStock(int quantity)
+        {
+            if(quantity<0)
+            {
+                throw new ArgumentException("Quantity cannot be Empty");
+            }
+            if(quantity>Stock)
+            {
+                throw new InvalidOperationException("Not enough stock available");
+            }
+            else
+            {
+                Stock -= quantity; 
+            }
+        }
+
+    }
+}
