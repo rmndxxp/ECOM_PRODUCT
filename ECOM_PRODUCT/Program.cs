ab2d054b6da5940ca81d6e7a9550041a7c0b590d
Ramandeep <ramandeep1568@conestogac.on.ca>
2024-06-01 01:47:10 -0400

    Initial commit of project setup

---
+++ ECOM_PRODUCT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ECOM_PRODUCT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Assignment 2 Group 10");
+            Console.ReadKey();
+        }
+    }
+}
